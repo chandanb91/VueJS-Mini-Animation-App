829c7469180f5eae02f5863eecd7e638b5dcd455
Chandan <chandan7791@gmail.com>
2018-09-27 17:02:36 +0530

    Initializing the Application

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 class="text-center">The Super Quiz</h1>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer
+  }
+};
+</script>
+
