6c432b7d1797a75a8575b031265026cf41c8ca81
Chandan <chandan7791@gmail.com>
2018-10-05 11:38:03 +0530

    Adding Animations

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,32 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Question from './components/Question.vue';
-import Answer from './components/Answer.vue';
+import Question from "./components/Question.vue";
+import Answer from "./components/Answer.vue";
 export default {
   data() {
     return {
-        mode: 'app-question'
+      mode: "app-question"
     };
   },
   methods: {
-      answered(isCorrect) {
-          if(isCorrect) {
-              this.mode = 'app-answer';
-          } else {
-              this.mode = 'app-question';
-              alert('Wrong Answer, Try Again!');
-          }
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "app-answer";
+      } else {
+        this.mode = "app-question";
+        alert("Wrong Answer, Try Again!");
       }
+    }
   },
   components: {
     appQuestion: Question,
@@ -41,6 +43,38 @@ export default {
 </script>
 
 <style>
+/* .flip-enter {
+     transform: rotateY(0deg); 
+} */
 
+.flip-enter-active {
+    animation: flip-in 0.5s ease-out forwards;
+}
+
+/* .flip-leave {
+    transform: rotateY(0deg);
+} */
+
+.flip-leave-active {
+    animation: flip-out 0.5s ease-out forwards;
+}
+
+@keyframes flip-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
+}
+
+@keyframes flip-in {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0deg);
+  }
+}
 </style>
 
